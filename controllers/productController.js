c693831d85d16018eee88cd285e936502903efbc
Drew <drc920630@gmail.com>
2023-04-10 16:44:42 +0900

    이미지 업로드 성공 완료!!!!

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,15 +1,21 @@
+/* eslint-disable max-len */
+/* eslint-disable object-curly-newline */
+/* eslint-disable dot-notation */
 require('../mongooseConnect');
 const Product = require('../models/product');
 
 // 상품 등록
 const createProduct = (req, res) => {
-  const { productName, price, size, color, category, quantity, detail, createAt } = req.body;
-  const imgMain = req.files['imgMain'][0].filename;
-  const imgSub1 = req.files['imgSub1'][0].filename;
-  const imgSub2 = req.files['imgSub2'][0].filename;
-  const imgSub3 = req.files['imgSub3'][0].filename;
-  const imgSub4 = req.files['imgSub4'][0].filename;
-  const imgSub5 = req.files['imgSub5'][0].filename;
+  console.log(req.body);
+  console.log(req.files);
+  const { productName, price, size, color, category, quantity, detail, createAt } = JSON.parse(req.body.data);
+
+  const img = req.files.map((el) => el.originalname);
+  // const imgSub1 = req.files['imgSub1'][0].filename;
+  // const imgSub2 = req.files['imgSub2'][0].filename;
+  // const imgSub3 = req.files['imgSub3'][0].filename;
+  // const imgSub4 = req.files['imgSub4'][0].filename;
+  // const imgSub5 = req.files['imgSub5'][0].filename;
 
   const newProduct = new Product({
     productName,
@@ -17,12 +23,7 @@ const createProduct = (req, res) => {
     size,
     color,
     category,
-    imgMain,
-    imgSub1,
-    imgSub2,
-    imgSub3,
-    imgSub4,
-    imgSub5,
+    img,
     quantity,
     detail,
     createAt,
@@ -31,11 +32,11 @@ const createProduct = (req, res) => {
   newProduct
     .save()
     .then(() => {
-      res.status(200).send('상품 등록 성공!');
+      res.status(200).json('상품 등록 성공!');
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('상품 등록 실패');
+      res.status(500).json('상품 등록 실패');
     });
 };
 
--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { loginUser } = require('../controllers/userController');
+// const { loginUser } = require('../controllers/userController');
 
 router.get('/', async (req, res) => {
   res.send('로그인 페이지입니다.');
 });
 
-router.post('/', loginUser);
+// router.post('/', loginUser);
 
 module.exports = router;
