0be9cf6196e1f9a721c633a3ebd9e5733789cbca
성희 <86584041+tjdgml97@users.noreply.github.com>
2023-04-10 13:54:42 +0900

    feat[#5]: order model생성

--- models/order.js
+++ models/order.js
@@ -1,19 +1,80 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
+
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   },
-  totalPrice: { type: Number },
-  productIds: [
+  name: {
+    type: String, 
+  },
+  address : {
+    type: String 
+  },
+  message : {
+    type: String,
+
+  },
+  phoneNumber : {
+    type: String,
+
+  },
+  points : {
+    type: Number,
+    default: 0 
+  },
+  totalPrice : {
+    type: Number,
+    default: 0 
+  },
+  isPaid : {
+    type: Boolean,
+    default: false,
+  },
+  orderDate : {
+    type: Date,
+    default: Date.now,
+  },
+
+  products : [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      color: {
+        type: String,
+      },
+      size: {
+        type: String,
+      },
+      quantity: {
+        type: Number,
+      },
+      image: {
+        type: String,
+      },
+      productName: {
+        type: String,
+      },
+      price: {
+        type: Number,
+      },
     },
-  ],
-  paymentType: { type: String, required: true },
-  createdDate: { type: Date },
+  ]
+
+  // productIds: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'Product',
+  //   },
+  // ],
+  // totalPrice: { type: Number },
+
+  // paymentType: { type: String, required: true },
+  // createdDate: { type: Date },
 });
 
 module.exports = mongoose.model('Order', orderSchema);
