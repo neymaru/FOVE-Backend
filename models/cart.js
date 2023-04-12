da14301050f450fe432ae57837d16c7c8ed338c6
neymaru <106804514+neymaru@users.noreply.github.com>
2023-04-12 10:18:04 +0900

    feat[#17] : Cart DB 최신화, userTest.js 삭제

--- models/cart.js
+++ models/cart.js
@@ -1,22 +1,19 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-  cartNo: { type: String, unique: true },
+  userId: { type: String },
   products: [
     {
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-      },
+      productName: { type: String },
       img: { type: String },
-      color: { type: String },
-      size: { type: String },
-      quantity: { type: Number },
       price: { type: Number },
+      size: { type: String },
+      color: { type: String },
+      quantity: { type: Number },
+      unitSumPrice: { type: Number },
     },
   ],
   totalPrice: { type: Number },
-  createAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Cart', cartSchema);
