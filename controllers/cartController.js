8288e74322b403e9ee169f8536f6778f0bada4e5
neymaru <106804514+neymaru@users.noreply.github.com>
2023-04-14 01:05:51 +0900

    feat[#25] 장바구니 카운팅

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,18 @@
 const Cart = require('../models/cart');
 const User = require('../models/user');
 
+// 장바구니 정보 조회(전체 상품 데이터, length)
+const getCartInfo = async (req, res) => {
+  try {
+    const cart = await Cart.findOne();
+    if (!cart) return res.status(200).json({ length: 0 }); // 장바구니가 비어있으면 lenght만 넘기기
+    res.status(200).json({ products: cart.products, length: cart.products.length }); // 장바구니에 상품이 있으면 상품 데이터와 length 넘기기
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('장바구니 조회 실패');
+  }
+};
+
 // 상품 상세페이지에서 해당상품 장바구니에 추가
 const addProductToCart = async (req, res) => {
   try {
@@ -36,22 +48,7 @@ const addProductToCart = async (req, res) => {
   }
 };
 
-// 장바구니 정보 조회(전체 상품 데이터, length)
-const getCartInfo = async (req, res) => {
-  try {
-    const cart = await Cart.findOne();
-    if (!cart) {
-      res.status(200).json({ length: 0 }); // 장바구니가 비어있으면 lenght만 넘기기
-    } else {
-      res.status(200).json({ products: cart.products, length: cart.products.length }); // 장바구니에 상품이 있으면 상품 데이터와 length 넘기기
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json('조회 실패');
-  }
-};
-
-// 장바구니 상품 한개 삭제
+// 장바구니 특정 상품 하나 삭제
 const removeCartItem = async (req, res) => {
   try {
     const { productId } = req.params;
@@ -95,6 +92,65 @@ const cleanCart = async (req, res) => {
   }
 };
 
+// 장바구니 상품 카운팅 + 1
+const cartProductQtyPlus = async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const cart = await Cart.findOne({});
+
+    if (!cart) {
+      return res.status(404).json('장바구니 없음');
+    }
+
+    const product = await cart.products.find((productEl) => productEl._id.toString() === productId); // productEl 의 _id 속성을 문자열로 변환해야 비교가 됨
+
+    if (!product) {
+      return res.status(404).json('일치하는 상품 없음');
+    }
+
+    product.quantity += 1;
+    product.unitSumPrice = product.price * product.quantity;
+
+    await cart.save();
+
+    res.status(200).json(cart);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('알 수 없는 에러');
+  }
+};
+
+// 장바구니 상품 카운팅 - 1
+const cartProductQtyMinus = async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const cart = await Cart.findOne({});
+
+    if (!cart) {
+      return res.status(404).json('장바구니 없음');
+    }
+
+    const product = await cart.products.find((productEl) => productEl._id.toString() === productId); // productEl 의 _id 속성을 문자열로 변환해야 비교가 됨
+
+    if (!product) {
+      return res.status(404).json('일치하는 상품 없음');
+    }
+
+    if (product.quantity === 0) return res.send('더이상 감소시킬 수 없음'); // quantity 가 0보다 작아지지 않도록 함
+    product.quantity -= 1;
+    product.unitSumPrice = product.price * product.quantity;
+
+    await cart.save();
+
+    res.status(200).json(cart);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('알 수 없는 에러');
+  }
+};
+
 // ------------------- 예비 코드 -------------------
 // 장바구니에 상품 추가
 // const addProductToCart = async (req, res) => {
@@ -125,4 +181,6 @@ module.exports = {
   getCartInfo,
   removeCartItem,
   cleanCart,
+  cartProductQtyPlus,
+  cartProductQtyMinus,
 };
