123521e2fb5798679ddddd86c447e64190011fd8
neymaru <106804514+neymaru@users.noreply.github.com>
2023-04-17 23:28:18 +0900

    feat[#40] : 유저장바구니 상품하나삭제, 비우기, 상품수량 증감

--- routes/cart.js
+++ routes/cart.js
@@ -9,22 +9,22 @@ const {
   cartProductQtyMinus,
 } = require('../controllers/cartController');
 
-// 장바구니 정보 불러오기 /cart/list/"유저아이디"
-router.post('/list/:userid', getCartInfo); // 장바구니 정보 불러오기(장바구니 전체 데이터, 장바구니 products length)
+// 장바구니 정보 불러오기 /cart/list/"유저id"
+router.post('/list/:userId', getCartInfo); // 장바구니 정보 불러오기(장바구니 전체 데이터, 장바구니 products length)
 
-// 장바구니에 상품 추가 /cart/add/"유저아이디"
-router.post('/add/:userid', addProductToCart);
+// 장바구니에 상품 추가 /cart/add/"유저id"
+router.post('/add/:userId', addProductToCart);
 
-// 장바구니 상품 하나 삭제 /cart/productId/"상품고유코드"
-router.post('/productId/:productId', removeCartItem);
+// 장바구니 상품 하나 삭제 /cart/remove/"유저id"/"상품고유코드"
+router.post('/remove/:userId/:productId', removeCartItem);
 
-// 장바구니 비우기 localhost:4000/cleancart
-router.post('/cleancart', cleanCart);
+// 장바구니 비우기 /cart/clean/"유저id"
+router.post('/clean/:userId', cleanCart);
 
-// 장바구니 상품 수량 증가 + 1 /cart/count/productId/"상품고유코드"
-router.post('/countplus/productId/:productId', cartProductQtyPlus);
+// 장바구니 상품 수량 증가 + 1 /cart/qtyplus/"유저id"/"상품고유코드"
+router.post('/qtyplus/:userId/:productId', cartProductQtyPlus);
 
-// 장바구니 상품 수량 감소 - 1 /cart/count/productId/"상품고유코드"
-router.post('/countminus/productId/:productId', cartProductQtyMinus);
+// 장바구니 상품 수량 감소 - 1 /cart/qtyminus/"유저id"/"상품고유코드"
+router.post('/qtyminus/:userId/:productId', cartProductQtyMinus);
 
 module.exports = router;
